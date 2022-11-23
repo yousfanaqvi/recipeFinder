194819f0de96adc3e36241bdb71ea7e1838d086e
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-11-23 18:21:01 -0500

    version 2.5b

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (_, res) {
